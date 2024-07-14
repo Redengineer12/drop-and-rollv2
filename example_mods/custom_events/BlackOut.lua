fa146840877f1bb323b3eda8fbb01ecefecc93a6
Redengineer12 <101785863+Redengineer12@users.noreply.github.com>
2024-07-14 20:45:04 +0700

    Add files via upload

---
+++ example_mods/custom_events/BlackOut.lua
@@ -0,0 +1,13 @@
+function onEvent(name, value1, value2)
+	if name == 'BlackOut' then
+		if value1 == 'true' then
+		makeLuaSprite('BlackFlash', 'BlackFlash', 0, 0);
+		scaleObject('BlackFlash', 18, 22);
+      setObjectCamera("BlackFlash", 'other')
+		addLuaSprite('BlackFlash', true)
+		setProperty('BlackFlash.visible', true);
+		elseif value1 == 'false' then
+		setProperty('BlackFlash.visible', false)
+			end
+		end
+	end
---
+++ example_mods/custom_events/RobberyFliplol.lua
@@ -0,0 +1,5 @@
+function onEvent(name, value1, value2)
+if name == 'RobberyFliplol' then
+ doTweenAngle('bruh', 'camera', 360, 1, 'expoInOut')
+end
+end
